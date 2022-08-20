d915b4e272c3039d52e5fc638ef13ef306aebc7e
Francisco Coutiño <sal23.1@gmail.com>
2022-08-20 14:57:24 -0500

    API:User:Register & Login

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
 
-Route::apiResource('products', ProductController::class);
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
