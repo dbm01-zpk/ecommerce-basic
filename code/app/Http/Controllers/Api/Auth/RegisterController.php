c6995d6fcdbc7b4d0e5c6f36e8d76affa57cb4c1
Francisco Coutiño <sal23.1@gmail.com>
2022-08-23 12:21:29 -0500

    Fix code error login and request laravel user store

--- code/app/Http/Controllers/Api/Auth/RegisterController.php
+++ code/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -8,8 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RegisterController extends BaseController
-{
+class RegisterController extends BaseController {
 
     /**
      * Register api
@@ -17,8 +16,7 @@ class RegisterController extends BaseController
      * @return \Illuminate\Http\Response
      */
 
-    public function register(StoreUserRequest $request)
-    {
+    public function register(StoreUserRequest $request) {
 
         $input             = $request->all();
         $input['password'] = bcrypt($input['password']);
@@ -36,8 +34,7 @@ class RegisterController extends BaseController
      * @return \Illuminate\Http\Response
      */
 
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user             = Auth::user();
@@ -46,7 +43,7 @@ class RegisterController extends BaseController
             return $this->sendResponse($success, 'User login successfully.');
 
         } else {
-            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised'], 401);
 
         }
 
